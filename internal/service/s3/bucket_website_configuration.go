1e6028a22285419d81cebdc9acb1fba4642a63a9
Max Leychenko <mleychenko@racktopsystems.com>
2024-07-02 12:44:33 -0400

    Seagate Lyve does not support GetBucketWebsite

--- internal/service/s3/bucket_website_configuration.go
+++ internal/service/s3/bucket_website_configuration.go
@@ -7,10 +7,12 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"net/http"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/smithy-go"
 	"github.com/hashicorp/aws-sdk-go-base/v2/tfawserr"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
@@ -417,6 +419,14 @@ func findBucketWebsite(ctx context.Context, conn *s3.Client, bucket, expectedBuc
 		return nil, &retry.NotFoundError{
 			LastError:   err,
 			LastRequest: input,
+		}
+	}
+
+	// Seagate Lyve does not support GetBucketWebsite but returns 403 instead of 501
+	if tfawserr.ErrCodeEquals(err, errCodeAccessDenied) {
+		return nil, &smithy.GenericAPIError{
+			Code:    errCodeNotImplemented,
+			Message: http.StatusText(http.StatusNotImplemented),
 		}
 	}
 
